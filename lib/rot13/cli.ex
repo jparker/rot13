8d46cd5b3ab175527435cf26d1159baf44abef6b
John Parker <jparker@urgetopunt.com>
2019-09-20 04:08:50 -0700

    added ex_doc and earmark

--- lib/rot13/cli.ex
+++ lib/rot13/cli.ex
@@ -1,4 +1,18 @@
 defmodule Rot13.CLI do
+  @moduledoc """
+  CLI for Rot13.
+  """
+
+  @doc """
+  Applies `Rot13.encode/1` to each command line argument and prints the results.
+  """
+
+  def main([]) do
+    IO.write """
+    usage: rot13 word [word ...]
+    """
+  end
+
   def main(argv) do
     argv
     |> Enum.map(&Rot13.encode/1)
